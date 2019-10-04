4b2b5499d350329a0a70af0c2175a2a4ddd2bcb4
Azyph <l.fernandez9317@gmail.com>
2019-10-04 13:41:30 +0200

    recolorV1.0

--- recolor_text.c
+++ recolor_text.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include "game.h"
 #include "game_io.h"
+#define SIZE 12
 
-int nbCoupMax = 0;
+struct game_s * game_default  () {
+    color cells [SIZE*SIZE] = { 0,0,0,2,0,2,1,0,1,0,3,0,
+                                0,3,3,1,1,1,1,3,2,0,1,0,
+                                1,0,1,2,3,2,3,2,0,3,3,2,
+                                2,3,1,0,3,2,1,1,1,2,2,0,
+                                2,1,2,3,3,3,3,2,0,1,0,0,
+                                0,3,3,0,1,1,2,3,3,2,1,3,
+                                1,1,2,2,2,0,0,1,3,1,1,2,
+                                1,3,1,3,1,0,1,0,1,3,3,3,
+                                0,3,0,1,0,0,2,1,1,1,3,0,
+                                1,3,1,0,0,0,3,2,3,1,0,0,
+                                1,3,3,1,1,2,2,3,2,0,0,2,
+                                2,0,2,3,0,1,1,1,2,3,0,1} ;
+    struct game_s * game = game_new (&cells[SIZE*SIZE] , 12) ;
+    return game ;
+}
 
-game_default() {
-  return game_new_empty();
+void game_display(cgame g){
+
+    printf("nb coups joués : %d ; nb coup max : %d \n", game_nb_moves_cur(g),game_nb_moves_max(g));
+
+    for (int y = 0; y<SIZE ; y++){
+
+        for (int x = 0 ; x<SIZE ; x++){
+
+            printf("%d", game_cell_current_color(g ,x ,y));
+
+        }printf("\n");
+
+    }printf("Jouer un coup: (0,1,2,3,r ou q ; r pour redémarrer ou q pour quitter)\n");
+
 }
-void game_display() {
-  printf("coucou");
-}
+
 
 int main(int argc, char* argv[])
 {
